d813ab427fd15cd2495dab2c7151cd050024c5f4
Jack <jack@crossproductionmedia.net>
2019-11-12 18:19:09 +0100

    Make the guest book work! you can save the names to a file and also retrive that file. That the names were in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import os
 
 root = tk.Tk()
 # Add elements here
@@ -8,14 +8,29 @@ def callback():
     text2 = tk.Label(root,text=name)
     text2.pack()
 
+def load():
+    f = open("names.txt","r")
+    contents = f.read()
+    text2 = tk.Label(root,text=contents)
+    text2.pack()
+    f.close()
+def save():
+    f = open("names.txt","a")
+    name = entry1.get()
+    writeme = name+"\n"
+    f.write(writeme)
 
 text1 = tk.Label(root,text="Please Enter your name.")
 entry1 = tk.Entry(root)
 button1 = tk.Button(root,text="Submit Name",command=callback)
+button2 = tk.Button(root,text="Load Names",command=load)
+button3 = tk.Button(root,text="Save Names",command=save)
 text1.pack()
 entry1.pack()
 button1.pack()
+button2.pack()
+button3.pack()
 
 root.geometry("500x400")
-root.title("Ip Ping machine")
+root.title("Guest Book")
 root.mainloop()
