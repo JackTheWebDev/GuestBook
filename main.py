5fd3855519f301bd2a7fe6f774b87399e09d2f02
Jack <jack@crossproductionmedia.net>
2019-11-12 18:03:45 +0100

    Made it kind of work - need to be able to load stuff from file

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import tkinter as tk
 root = tk.Tk()
 # Add elements here
 def callback():
-    ip = entry1.get()
-    print(ip)
+    name = entry1.get()
+    text2 = tk.Label(root,text=name)
+    text2.pack()
 
 
-text1 = tk.Label(root,text="Enter a Ip Address to pingj")
+text1 = tk.Label(root,text="Please Enter your name.")
 entry1 = tk.Entry(root)
 button1 = tk.Button(root,text="Submit Name",command=callback)
 text1.pack()
@@ -18,4 +19,3 @@ button1.pack()
 root.geometry("500x400")
 root.title("Ip Ping machine")
 root.mainloop()
-# Hello Gamers
